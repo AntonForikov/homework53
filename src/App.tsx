8fdad13826308708df4987a2dfc42dabacf3b2f7
Anton Forikov <tohabass@gmail.com>
2023-12-31 11:41:16 +0300

    remove trash styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {ReactNode, useState} from 'react';
+import './App.css';
+import AddTaskForm from "./AddTaskForm/AddTaskForm.tsx";
+import Task from "./Task/Task.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface State {
+    task: string,
+    id: null | string
 }
 
-export default App
+function App() {
+    const [todo, setTodo] = useState<State[]>([
+        {task: "Make homework", id: null},
+        {task: "Take shower", id: null},
+        {task: "Make koffee", id: null},
+    ])
+
+    const deleteTask = (index: number) => {
+        const taskCopy = [...todo];
+        taskCopy.splice(index, 1);
+        setTodo([...taskCopy]);
+    };
+
+    const todoList: ReactNode = (<>
+        {todo.map((task, index) => {
+            task.id = index.toString()
+            return <Task todo={task.task} removeTask={() => deleteTask(index)} key={task.id}/>
+        })}
+    </>);
+
+    let message = "";
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        message = event.target.value;
+    }
+
+    const setMessage = () => {
+      const todoCopy = [...todo];
+      if (message.length > 0) {
+        const newTodo = {task: message, id: todo.length.toString()}
+        setTodo([newTodo, ...todoCopy])
+      } else {
+        alert("Write something to add new message!")
+      }
+    }
+
+    return (
+        <>
+            <AddTaskForm
+                handleChange={(event) => handleChange(event)}
+                setMessage={setMessage}/>
+            {todoList}
+        </>
+    )
+}
+
+export default App;
